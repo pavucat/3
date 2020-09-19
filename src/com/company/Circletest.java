9e1c993b026c6136104c4df426b6b990a1d5dc98
FoPQer <iritskiy.sas@gmail.com>
2020-09-19 15:27:34 +0300

    11

--- src/com/company/Circletest.java
+++ src/com/company/Circletest.java
@@ -2,15 +2,40 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Circletest {
+public class  Circletest {
 
     public static void main(String[] args) {
-        circle one=new circle();
+        /*circle one=new circle();
         Scanner in=new Scanner(System.in);
         double r=in.nextDouble();
         one.setR(r);
         double S= one.getS();
         double l=one.getL();
-        System.out.println("S= "+S+", l= "+l);
+        System.out.println("S= "+S+", l= "+l);*/
+
+        /*leg one = new leg();
+        hand two = new hand();
+        head three = new head();
+        Scanner in=new Scanner(System.in);
+        int l1=in.nextInt();
+        boolean b=in.nextBoolean();
+        int l2=in.nextInt();
+        int s1=in.nextInt();
+        int l3=in.nextInt();
+        one.setLengh(l1);
+        one.setFlatfeet(b);
+        two.setHandlengh(l2);
+        two.setPalmsquare(s1);
+        three.setEyecolor("rozovii");
+        three.setVision(l3);
+        System.out.println(one.getLengh());*/
+
+
+        book one = new book();
+        one.setAge(1992);
+        one.setAuthor("1111");
+        one.setColor("krasivii");
+        one.setStranitsi(1000);
+        System.out.println(one.getAuthor());
     }
 }
--- src/com/company/human.java
+++ src/com/company/human.java
@@ -7,10 +7,59 @@ class leg extends human{
     int lengh;
     Boolean flatfeet;
 
+    public void setFlatfeet(Boolean flatfeet) {
+        this.flatfeet = flatfeet;
+    }
+
+    public void setLengh(int lengh) {
+        this.lengh = lengh;
+    }
+
+    public int getLengh() {
+        return lengh;
+    }
+
+    public Boolean getFlatfeet() {
+        return flatfeet;
+    }
 }
 class hand extends human{
+int handlengh;
+int palmsquare;
 
+    public void setHandlengh(int handlengh) {
+        this.handlengh = handlengh;
+    }
+
+    public void setPalmsquare(int palmsquare) {
+        this.palmsquare = palmsquare;
+    }
+
+    public int getHandlengh() {
+        return handlengh;
+    }
+
+    public int getPalmsquare() {
+        return palmsquare;
+    }
 }
 class head extends human{
+double vision;
+String eyecolor;
 
+    public void setEyecolor(String eyecolor) {
+        this.eyecolor = eyecolor;
+    }
+
+    public void setVision(double vision) {
+        this.vision = vision;
+    }
+
+    public double getVision() {
+        return vision;
+    }
+
+    public String getEyecolor() {
+        return eyecolor;
+    }
 }
